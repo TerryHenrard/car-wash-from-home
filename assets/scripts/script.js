c212aa05947ca086cc6b47f2a2fd24cf23fdd1ac
TerryHenrard <henrardterry2203@hotmail.com>
2023-06-26 14:59:28 +0200

    header animations done

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,8 @@
+const body = document.querySelector("body");
+const h1MainTitle = document.querySelector(".h1-main-title");
+const h2MainTitle = document.querySelector(".h2-main-title");
+const pMainTitle = document.querySelector(".p-main-title");
+
 let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
@@ -12,4 +17,12 @@ window.addEventListener("scroll", () => {
 
 navbar.addEventListener("mouseover", () => {
   navbar.style.top = 50 + "px";
+});
+
+window.addEventListener("load", () => {
+  body.style.opacity = "1";
+  h1MainTitle.classList.add("h1-anim");
+  h2MainTitle.classList.add("h2-anim");
+  pMainTitle.classList.add("p-anim");
+  navbar.classList.add("nav-anim");
 });
